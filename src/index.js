b7c3988f0b6bc52c25e3da8f4457d736bc2a03e1
Maria Dayekh <mariaaldayekh@gmail.com>
2022-11-27 13:33:34 +0200

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
+// OO solution
+class Task {
+  constructor(description) {
+    this.description = description;
+  }
+
+  render() {
+    return `
+      <li>
+        ${this.description}
+      </li>
+      `;
+  }
+}
+class TaskList {
+  constructor() {
+    this.tasks = [];
+  }
+
+  createNewTask(description) {
+    const newTask = new Task(description);
+    this.tasks.push(newTask);
+  }
+
+  renderTasks() {
+    return this.tasks.map((task) => task.render()).join("");
+  }
+
+
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskList = new TaskList();
+  
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  
+  const taskUl = document.getElementById("tasks");
+
+  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
+
+
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskList.createNewTask(newTaskDescription.value);
+    // reset form
+    e.target.reset();
+    renderApp();
+  });
+
 });
